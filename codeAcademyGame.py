71f41a1f2f5aec312118f6264e9e658a4f100621
Norbikx1 <norbikx211@gmail.com>
2023-02-18 20:37:26 +0000

    Added getData method

---
+++ codeAcademyGame.py
@@ -0,0 +1,12 @@
+#Mortgage Calculator by Norbert
+
+def getData(amount, perecentage, years):
+    
+    print ("How much money do you need?") 
+    amount = input()
+    print("What is the perecentage of the loan?")
+    perecentage = input()
+    print("For how many year do you want to take the loan?")
+    years = input()
+
+    
